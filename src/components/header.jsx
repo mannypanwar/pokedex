bdf1817f6120229db90b7336c8a9a374e7121125
Mandeep Singh Panwar <47856939+mannypanwar@users.noreply.github.com>
2020-06-19 13:12:52 +0530

    added bg-light to navbar class

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <nav className="navbar ">
+    <nav className="navbar bg-light">
       <a className="navbar-brand" href="/">
         <img src="images/pokeball.png" alt="" loading="lazy" />
       </a>
